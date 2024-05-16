20dfc50e67a80d6f88196c612c693fb1b1ba4d71
kukuh2307 <kukuhagung12@gmail.com>
2024-05-16 23:34:11 +0700

    fix login layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,9 +22,7 @@
                 </a>
             </div>
 
-            <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md sm:rounded-lg">
-                {{ $slot }}
-            </div>
+            {{ $slot }}
         </div>
     </body>
 </html>
