c8166069a46d5665a06b66bfff321d2d71b00a55
kukuh2307 <kukuhagung12@gmail.com>
2024-05-22 21:57:46 +0700

    finish:make url livewire kelas

---
+++ app/Livewire/Kelas/KelasKelasTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class KelasKelasTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.kelas-kelas-table');
+    }
+}
