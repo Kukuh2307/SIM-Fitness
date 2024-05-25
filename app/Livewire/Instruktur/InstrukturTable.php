cb7c6d74db1a58c428f8dc475f93f04fff1ff6ee
kukuh2307 <kukuhagung12@gmail.com>
2024-05-25 12:03:45 +0700

    finish:view instruktur input and tabe

--- app/Livewire/Instruktur/InstrukturTable.php
+++ app/Livewire/Instruktur/InstrukturTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Instruktur;
 
+use App\Models\Instruktur;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,7 +12,7 @@ class InstrukturTable extends Component
 
     public function render()
     {
-        $instrukturs = \App\Models\Instruktur::orderBy('created_at', 'desc')->paginate(10);
+        $instrukturs = Instruktur::orderBy('created_at', 'desc')->paginate(10);
         return view('livewire.instruktur.instruktur-table', [
             'instrukturs' => $instrukturs
         ]);
