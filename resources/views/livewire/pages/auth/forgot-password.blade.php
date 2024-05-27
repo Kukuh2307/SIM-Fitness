bc94952023540074cc0b3366c0ded7344be13335
Joseph Victorio <136730548+Joseph-Victorio@users.noreply.github.com>
2024-05-27 12:41:21 +0700

    RENEW FORGOT PASS UI

--- resources/views/livewire/pages/auth/forgot-password.blade.php
+++ resources/views/livewire/pages/auth/forgot-password.blade.php
@@ -36,26 +36,33 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form wire:submit="sendPasswordResetLink">
-        <!-- Email Address -->
+<div class="from-red-600 to-slate-800 bg-gradient-to-tr backdrop-blur-md w-screen h-screen">
+    <div class="rounded-xl shadow-lg border-black border-1 shadow-slate-900 p-5 bg-white sm:w-[500px] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <p class="text-xl font-bold ">Reset Password</p>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="mb-4 text-sm ">
+            {{ __('Lupa password? Tuliskan email anda untuk dikirimkan reset link') }}
         </div>
-    </form>
+    
+        <!-- Session Status -->
+        <x-auth-session-status class="mb-4" :status="session('status')" />
+    
+        <form wire:submit="sendPasswordResetLink">
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" class="text-white" />
+                <x-text-input wire:model="email" id="email" class="block mt-1  w-full" type="email" name="email" required autofocus />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+    
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button class="bg-red-600 hover:bg-red-700 active:bg-red-800">
+                    {{ __('Email Password Reset Link') }}
+                </x-primary-button>
+            </div>
+        </form>
+    </div>
 </div>
