0d93c7d07d78e24ffaa64b63c1399daa574aa8a2
Ni Made Julia Budiantari <juliafemale1208@gmail.com>
2024-05-29 23:07:00 +0800

    memperbaiki bug create

--- resources/views/livewire/instruktur/create-instruktur.blade.php
+++ resources/views/livewire/instruktur/create-instruktur.blade.php
@@ -1,4 +1,4 @@
-<div class="py-4 space-y-6">
+<div class="py-4">
     @if ($errors->any())
         @foreach ($errors->all() as $error)
             <div x-data="{ show: true }" x-show="show" x-init="setTimeout(() => show = false, 3000)" class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg alert dark:bg-red-200 dark:text-red-800" role="alert">
@@ -14,40 +14,34 @@
 
     <form wire:submit.prevent="{{ $btnUpdate ? 'update' : 'store' }}">
         @csrf
-        <div class="grid grid-cols-1 gap-6 md:grid-cols-2">
-            <div class="flex flex-col gap-3">
-                <label for="nama" class="font-semibold">Nama</label>
-                <input type="text" id="nama" name="nama" wire:model.lazy="nama" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Nama">
+        <div class="flex items-start gap-4">
+            <div class="flex flex-col w-full gap-3 md:w-1/2">
+                <input type="text" name="nama" wire:model.lazy="nama" class="w-full p-2 text-black bg-white rounded-lg" placeholder="Nama">
                 @error('nama') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
 
-                <label for="email" class="font-semibold">Email</label>
-                <input type="email" id="email" name="email" wire:model.lazy="email" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Email">
+                <input type="email" name="email" wire:model.lazy="email" class="w-full p-2 text-black bg-white rounded-lg" placeholder="Email">
                 @error('email') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
 
-                <label for="deskripsi" class="font-semibold">Deskripsi</label>
-                <textarea id="deskripsi" name="deskripsi" wire:model.lazy="deskripsi" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Deskripsi" cols="2" rows="2"></textarea>
+                <textarea name="deskripsi" wire:model.lazy="deskripsi" class="w-full p-2 text-black bg-white rounded-lg" placeholder="Deskripsi" cols="5" rows="5"></textarea>
                 @error('deskripsi') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
             </div>
-
-            <div class="flex flex-col gap-3">
-                <label for="spesialis" class="font-semibold">Spesialis</label>
-                <input type="text" id="spesialis" name="spesialis" wire:model.lazy="spesialis" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Spesialis">
+            <div class="flex flex-col w-full gap-3 md:w-1/2">
+                <input type="text" name="spesialis" wire:model.lazy="spesialis" class="w-full p-2 text-black bg-white rounded-lg" placeholder="Spesialis">
                 @error('spesialis') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
 
-                <label for="biaya" class="font-semibold">Biaya</label>
-                <input type="number" id="biaya" name="biaya" wire:model.lazy="biaya" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent" placeholder="Biaya">
+                <input type="number" name="biaya" wire:model.lazy="biaya" class="w-full p-2 text-black bg-white rounded-lg" placeholder="Biaya">
                 @error('biaya') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
-
-                <label for="foto" class="font-semibold">Foto</label>
-                <input type="file" id="foto" name="foto" wire:model="foto" class="w-full p-3 text-black rounded-lg border border-gray-200 focus:ring-2 focus:ring-red-600 focus:border-transparent">
                 @if($btnUpdate)
+                    <input type="file" name="foto" wire:model="foto" class="w-full p-2 text-black bg-white rounded-lg" value="{{ $foto }}">
                     <img src="{{ asset('storage/'.$fotoLama) }}" class="w-32 h-32 mt-2 rounded-md" alt="Foto Instruktur">
+                @else
+                    <input type="file" name="fotoLama" wire:model="fotoLama" class="w-full p-2 text-black bg-white rounded-lg">
                 @endif
                 @error('foto') <span class="text-sm text-red-600">{{ $message }}</span> @enderror
             </div>
         </div>
-        <div class="flex justify-end mt-6">
-            <button type="submit" class="px-6 py-2 text-white bg-red-600 rounded-full hover:bg-red-700 focus:ring-2 focus:ring-red-600 focus:ring-offset-2">
+        <div class="flex justify-end mt-3">
+            <button type="submit" class="px-4 py-2 text-white bg-red-600 rounded-full">
                 {{ $btnUpdate ? 'Update' : 'Create' }}
             </button>
         </div>
