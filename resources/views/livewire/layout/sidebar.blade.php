0749600595fa3ba5782281bea15b2cfa33b68923
Ni Made Julia Budiantari <juliafemale1208@gmail.com>
2024-05-29 22:51:19 +0800

    menambahkan icon di sidebar dan mengganti logo

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -1,31 +1,35 @@
-<div class="fixed w-64 h-screen text-white from-red-600 to-slate-900 bg-gradient-to-tr backdrop-blur-md">
-    <div class="flex items-center">
-        <img src="{{ asset('images/logo.png') }}" alt="Logo Fit Banget">
+<div class="fixed w-64 h-screen text-white from-red-600 to-slate-800 bg-gradient-to-tr backdrop-blur-md">
+    <div class="flex items-center justify-center p-4">
+        <img src="{{ asset('images/baru.png') }}" alt="Logo Fit Banget" class="w-45 h-auto">
     </div>
     <ul class="mt-10">
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('dashboard') }}" class="w-full text-center">Dashboard</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/dashboard.png') }}" alt="Dashboard Icon" class="w-10 mr-5">
+            <a href="{{ url('dashboard') }}" class="w-full text-left">Dashboard</a>
         </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('members') }}" class="w-full text-center">Members</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/member.png') }}" alt="Members Icon" class="w-10 mr-5">
+            <a href="{{ url('members') }}" class="w-full text-left">Members</a>
         </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('list-alat') }}" class="w-full text-center">Listing Alat</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/listalat.png') }}" alt="Listing Alat Icon" class="w-10 mr-5">
+            <a href="{{ url('list-alat') }}" class="w-full text-left">Listing Alat</a>
         </li>
-        {{-- <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('metode-pembayaran') }}" class="w-full text-center">Metode Pembayaran</a> --}}
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/instruktur.png') }}" alt="Instruktur Icon" class="w-10 mr-5">
+            <a href="{{ url('instruktur') }}" class="w-full text-left">Instruktur</a>
         </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('instruktur') }}" class="w-full text-center">Instruktur</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/transaksi.png') }}" alt="Transaksi Icon" class="w-10 mr-5">
+            <a href="{{ url('transaksi') }}" class="w-full text-left">Transaksi</a>
         </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('transaksi') }}" class="w-full text-center">Transaksi</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/class.png') }}" alt="Kelas Icon" class="w-10 mr-5">
+            <a href="{{ url('kelas') }}" class="w-full text-left">kelas</a>
         </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('kelas') }}" class="w-full text-center">kelas</a>
-        </li>
-        <li class="flex items-center justify-center px-4 py-5 text-xl font-bold hover:bg-red-700">
-            <a href="{{ url('informasi') }}" class="w-full text-center">Informasi</a>
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+            <img src="{{ asset('images/informasi.png') }}" alt="Informasi Icon" class="w-10 mr-5">
+            <a href="{{ url('informasi') }}" class="w-full text-left">Informasi</a>
         </li>
     </ul>
 </div>
