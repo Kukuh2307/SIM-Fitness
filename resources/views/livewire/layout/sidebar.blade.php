be4709ddd71b730ba6d805f094a895dcbfb3d3a5
kukuh2307 <kukuhagung12@gmail.com>
2024-05-31 10:09:27 +0700

    modify: active sidebar

--- resources/views/livewire/layout/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar.blade.php
@@ -3,31 +3,31 @@
         <img src="{{ asset('images/baru.png') }}" alt="Logo Fit Banget" class="h-auto w-45">
     </div>
     <ul class="mt-10">
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Dashboard' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/dashboard.png') }}" alt="Dashboard Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('dashboard') }}" class="w-full text-left">Dashboard</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Members' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/member.png') }}" alt="Members Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('members') }}" class="w-full text-left">Members</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Listing Alat' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/listalat.png') }}" alt="Listing Alat Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('list-alat') }}" class="w-full text-left">Listing Alat</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Instruktur' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/instruktur.png') }}" alt="Instruktur Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('instruktur') }}" class="w-full text-left">Instruktur</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Transaksi' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/transaksi.png') }}" alt="Transaksi Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('transaksi') }}" class="w-full text-left">Transaksi</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Kelas' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/class.png') }}" alt="Kelas Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('kelas') }}" class="w-full text-left">Kelas</a>
         </li>
-        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700">
+        <li class="flex items-center justify-start px-10 py-5 text-xl font-bold hover:bg-red-700 {{ $title == 'Informasi' ? 'bg-red-700' : '' }}">
             <img src="{{ asset('images/informasi.png') }}" alt="Informasi Icon" class="w-10 mr-5 filter invert">
             <a href="{{ url('informasi') }}" class="w-full text-left">Informasi</a>
         </li>
